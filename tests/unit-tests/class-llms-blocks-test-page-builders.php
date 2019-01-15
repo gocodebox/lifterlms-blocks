ef172e646fe22917e29328bc8817c2f173e82c2b
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-01-15 11:21:00 -0800

    add tests for elementor conditions

--- tests/unit-tests/class-llms-blocks-test-page-builders.php
+++ tests/unit-tests/class-llms-blocks-test-page-builders.php
@@ -4,16 +4,17 @@
  *
  * @package LifterLMS_Blocks/Tests
  * @since   1.2.0
- * @version 1.2.0
+ * @version [version]
  */
 class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 
 	/**
 	 * Test whether or not filters are properly added based on the presence of the page builder.
 	 *
+	 * @runInSeparateProcess
 	 * @return  void
 	 * @since   1.2.0
-	 * @version 1.2.0
+	 * @version [version]
 	 */
 	public function test_add_filters() {
 
@@ -72,6 +73,34 @@ class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 		// PB is explicitly on, remove the actions.
 		update_post_meta( $course_id, '_et_pb_use_builder', 'on' );
 		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_divi( true, $course_id ) );
+
+	}
+
+	/**
+	 * Check the status of a the Elementor page builder.
+	 *
+	 * @return  void
+	 * @since   [version]
+	 * @version [version]
+	 */
+	public function test_check_for_elementor() {
+
+		$course_id = $this->factory->course->create();
+
+		// No value set, pb explicitly off, add actions.
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_elementor( true, $course_id ) );
+
+		// PB has non-standard values (assume off).
+		update_post_meta( $course_id, '_elementor_edit_mode', 'fake' );
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_elementor( true, $course_id ) );
+		update_post_meta( $course_id, '_elementor_edit_mode', 'yes' );
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_elementor( true, $course_id ) );
+		update_post_meta( $course_id, '_elementor_edit_mode', 'no' );
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_elementor( true, $course_id ) );
+
+		// PB is explicitly on, remove the actions.
+		update_post_meta( $course_id, '_elementor_edit_mode', 'builder' );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_elementor( true, $course_id ) );
 
 	}
 
