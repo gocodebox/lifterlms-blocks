c0bfd37d4ead327c53ef2e51b675677cfe295e4c
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-01-23 12:05:43 -0800

    new and more tests

--- tests/unit-tests/class-llms-blocks-test-functions.php
+++ tests/unit-tests/class-llms-blocks-test-functions.php
@@ -4,7 +4,7 @@
  *
  * @package LifterLMS_Blocks/Tests
  * @since   1.3.0
- * @version 1.3.1
+ * @version [version]
  */
 class LLMS_Blocks_Test_Functions extends LLMS_Blocks_Unit_Test_Case {
 
@@ -13,31 +13,50 @@ class LLMS_Blocks_Test_Functions extends LLMS_Blocks_Unit_Test_Case {
 	 *
 	 * @return  void
 	 * @since   1.3.0
-	 * @version 1.3.0
+	 * @version [version]
 	 */
 	public function test_llms_blocks_is_classic_enabled_for_post() {
 
 		$post_id = $this->factory->post->create();
 
-		// Classic editor isn't active.
+		// Classic editor defaults (classic ed. enabled for all users).
+		$this->assertTrue( llms_blocks_is_classic_enabled_for_post( $post_id ) );
+
+		// Enable block ed for all users.
+		$this->update_classic_settings( array( 'editor' => 'block' ) );
 		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
 
-		// Mock the Classic_Editor class
-		$classic = $this->getMockBuilder( 'Classic_Editor' )->getMock();
+		// Classic default, users can choose.
+		$this->update_classic_settings( array( 'editor' => 'classic', 'allow-users' => 'allow' ) );
 
-		// No postmeta.
+		// No postmeta set (block editor is used by default despite what the setting seems to indicate).
 		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
 
-		// Wrong postmeta.
-		update_post_meta( $post_id, 'classic-editor-remember', 'fake' );
-		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
+		// classic explicitly enabled.
+		update_post_meta( $post_id, 'classic-editor-remember', 'classic-editor' );
+		$this->assertTrue( llms_blocks_is_classic_enabled_for_post( $post_id ) );
 
-		// Block editor enabled.
+		// Block explicitly enabled.
 		update_post_meta( $post_id, 'classic-editor-remember', 'block-editor' );
 		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
 
-		// Classic Editor Enabled.
+		// Enable block by default.
+		$this->update_classic_settings( array( 'editor' => 'block', 'allow-users' => 'allow' ) );
+		delete_post_meta( $post_id, 'classic-editor-remember' );
+
+		// No postmeta set (block editor is used by default).
+		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
+
+		// classic explicitly enabled.
 		update_post_meta( $post_id, 'classic-editor-remember', 'classic-editor' );
+		$this->assertTrue( llms_blocks_is_classic_enabled_for_post( $post_id ) );
+
+		// Block explicitly enabled.
+		update_post_meta( $post_id, 'classic-editor-remember', 'block-editor' );
+		$this->assertFalse( llms_blocks_is_classic_enabled_for_post( $post_id ) );
+
+		// Enable block ed for all users, even with block-editor meta classic will still be used.
+		$this->update_classic_settings( array( 'editor' => 'classic' ) );
 		$this->assertTrue( llms_blocks_is_classic_enabled_for_post( $post_id ) );
 
 	}
@@ -46,9 +65,12 @@ class LLMS_Blocks_Test_Functions extends LLMS_Blocks_Unit_Test_Case {
 	 * Test the llms_blocks_is_post_migrated() method
 	 * @return  void
 	 * @since   1.3.1
-	 * @version 1.3.1
+	 * @version [version]
 	 */
 	public function test_llms_blocks_is_post_migrated() {
+
+		// Block editor on.
+		$this->update_classic_settings( array( 'editor' => 'block' ) );
 
 		// Post to test against.
 		$post_id = $this->factory->post->create();
@@ -64,19 +86,14 @@ class LLMS_Blocks_Test_Functions extends LLMS_Blocks_Unit_Test_Case {
 		update_post_meta( $post_id, '_llms_blocks_migrated', 'no' );
 		$this->assertFalse( llms_blocks_is_post_migrated( $post_id ) );
 
-		// Add the classic editor.
-		$classic = $this->getMockBuilder( 'Classic_Editor' )->getMock();
+		// Classic editor on.
+		$this->update_classic_settings( array( 'editor' => 'classic' ) );
 
-		// Block editor enabled & not migrated.
-		update_post_meta( $post_id, 'classic-editor-remember', 'block-editor' );
+		// Classic editor enabled & not migrated.
 		$this->assertFalse( llms_blocks_is_post_migrated( $post_id ) );
 
-		// Block editor enabled & migrated.
+		// Classic editor enabled & migrated.
 		update_post_meta( $post_id, '_llms_blocks_migrated', 'yes' );
-		$this->assertTrue( llms_blocks_is_post_migrated( $post_id ) );
-
-		// Classic Editor Enabled & migrated.
-		update_post_meta( $post_id, 'classic-editor-remember', 'classic-editor' );
 		$this->assertFalse( llms_blocks_is_post_migrated( $post_id ) );
 
 		// Classic Editor enabled & not migrated.
--- tests/unit-tests/class-llms-blocks-test-migrate.php
+++ tests/unit-tests/class-llms-blocks-test-migrate.php
@@ -4,7 +4,7 @@
  *
  * @package LifterLMS_Blocks/Tests
  * @since   1.3.1
- * @version 1.3.1
+ * @version [version]
  */
 class LLMS_Blocks_Test_Migrate extends LLMS_Blocks_Unit_Test_Case {
 
@@ -49,6 +49,62 @@ class LLMS_Blocks_Test_Migrate extends LLMS_Blocks_Unit_Test_Case {
 		update_post_meta( $post_id, '_llms_sales_page_content_type', 'none' );
 		$this->assertTrue( LLMS_Blocks_Migrate::check_sales_page( true, $post_id ) );
 
+	}
+
+	/**
+	 * Test get_migrateable_post_types() method
+	 *
+	 * @return  void
+	 * @since   [version]
+	 * @version [version]
+	 */
+	public function test_get_migrateable_post_types() {
+
+		$class = new LLMS_Blocks_Migrate();
+		$this->assertEquals( array( 'course', 'lesson' ), $class->get_migrateable_post_types() );
+
+	}
+
+	/**
+	 * Test should_migrate_post() method
+	 *
+	 * @return  void
+	 * @since   [version]
+	 * @version [version]
+	 */
+	public function test_should_migrate_post() {
+
+		$class = new LLMS_Blocks_Migrate();
+		$this->update_classic_settings( array( 'editor' => 'block', 'allow-users' => false ) );
+
+		// test various post types
+		$types = array(
+			'post' => false,
+			'page' => false,
+			'course' => true,
+			'lesson' => true,
+			'llms_membership' => false,
+		);
+		foreach ( $types as $type => $expect ) {
+
+			$id = $this->factory->post->create( array( 'post_type' => $type ) );
+			$this->assertEquals( $expect, $class->should_migrate_post( $id ) );
+
+		}
+
+		$id = $this->factory->post->create( array( 'post_type' => 'course' ) );
+
+		// Classic ed enabled, don't migrate.
+		$this->update_classic_settings( array( 'editor' => 'classic', 'allow-users' => false ) );
+		$this->assertFalse( $class->should_migrate_post( $id ) );
+
+		// Block enabled, go.
+		$this->update_classic_settings( array( 'editor' => 'block', 'allow-users' => false ) );
+		$this->assertTrue( $class->should_migrate_post( $id ) );
+
+		// already migrated
+		update_post_meta( $id, '_llms_blocks_migrated', 'yes' );
+		$this->assertFalse( $class->should_migrate_post( $id ) );
 
 	}
 
