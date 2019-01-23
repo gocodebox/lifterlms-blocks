1e53511c71c17cf987b10666a6a9d32034956f03
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-01-23 14:38:10 -0800

    add tests for divi & bb functions

--- tests/unit-tests/class-llms-blocks-test-page-builders.php
+++ tests/unit-tests/class-llms-blocks-test-page-builders.php
@@ -4,7 +4,7 @@
  *
  * @package LifterLMS_Blocks/Tests
  * @since   1.2.0
- * @version 1.3.1
+ * @version [version]
  */
 class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 
@@ -14,7 +14,7 @@ class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 	 * @runInSeparateProcess
 	 * @return  void
 	 * @since   1.2.0
-	 * @version 1.3.1
+	 * @version [version]
 	 */
 	public function test_add_filters() {
 
@@ -25,6 +25,7 @@ class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 		define( 'ET_BUILDER_VERSION', 1 );
 		LLMS_Blocks_Page_Builders::add_filters();
 		$this->assertEquals( 15, has_filter( 'llms_blocks_is_post_migrated', array( 'LLMS_Blocks_Page_Builders', 'check_for_divi' ) ) );
+		$this->assertEquals( 15, has_filter( 'llms_blocks_is_classic_enabled_for_post', array( 'LLMS_Blocks_Page_Builders', 'check_for_divi_classic' ) ) );
 
 		// Check for Elementor.
 		LLMS_Blocks_Page_Builders::add_filters();
@@ -41,6 +42,36 @@ class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 		define( 'FL_BUILDER_VERSION', 1 );
 		LLMS_Blocks_Page_Builders::add_filters();
 		$this->assertEquals( 15, has_filter( 'llms_blocks_is_post_migrated', array( 'LLMS_Blocks_Page_Builders', 'check_for_beaver' ) ) );
+
+	}
+
+	/**
+	 * Check the status of a the Elementor page builder.
+	 *
+	 * @return  void
+	 * @since   [version]
+	 * @version [version]
+	 */
+	public function test_check_for_beaver() {
+
+		global $llms_blocks_mock_fl_builder_enabled;
+		$llms_blocks_mock_fl_builder_enabled = null;
+
+		$course_id = $this->factory->course->create();
+
+		// No value set, pb explicitly off, add actions.
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_beaver( true, $course_id ) );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_beaver( false, $course_id ) );
+
+		// Explicitly off.
+		$llms_blocks_mock_fl_builder_enabled = false;
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_beaver( true, $course_id ) );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_beaver( false, $course_id ) );
+
+		// PB is explicitly on, remove the actions.
+		$llms_blocks_mock_fl_builder_enabled = true;
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_beaver( true, $course_id ) );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_beaver( false, $course_id ) );
 
 	}
 
@@ -73,6 +104,34 @@ class LLMS_Blocks_Test_Page_Builders extends LLMS_Blocks_Unit_Test_Case {
 		// PB is explicitly on, remove the actions.
 		update_post_meta( $course_id, '_et_pb_use_builder', 'on' );
 		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_divi( true, $course_id ) );
+
+	}
+
+	/**
+	 * Check the status of a the Divi page builder.
+	 *
+	 * @return  void
+	 * @since   [version]
+	 * @version [version]
+	 */
+	public function test_check_for_divi_classic() {
+
+		global $llms_blocks_mock_et_options;
+		$course_id = $this->factory->course->create();
+
+		// Classic mode is not defined (therefore off).
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_divi_classic( true, $course_id ) );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_divi_classic( false, $course_id ) );
+
+		// Explicitly off.
+		$llms_blocks_mock_et_options['et_enable_classic_editor'] = 'off';
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_divi_classic( true, $course_id ) );
+		$this->assertFalse( LLMS_Blocks_Page_Builders::check_for_divi_classic( false, $course_id ) );
+
+		// Explicitly on.
+		$llms_blocks_mock_et_options['et_enable_classic_editor'] = 'on';
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_divi_classic( true, $course_id ) );
+		$this->assertTrue( LLMS_Blocks_Page_Builders::check_for_divi_classic( false, $course_id ) );
 
 	}
 
