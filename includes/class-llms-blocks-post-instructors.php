6bbc14105e7f4b0cbac853e87b280f126e464af3
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-09-27 13:06:56 -0700

    automatically set instructor data during course/membership creation, fixes gocodebox/lifterlms/#937

--- includes/class-llms-blocks-post-instructors.php
+++ includes/class-llms-blocks-post-instructors.php
@@ -3,31 +3,47 @@
  * Handle course & membership instructors data.
  *
  * @package  LifterLMS_Blocks/Classes
- * @since    1.0.0
- * @version  1.0.0
+ *
+ * @since 1.0.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
 
 /**
  * Handle course & membership instructors data.
+ *
+ * @since 1.0.0
+ * @since [version] Automatically store course/membership instructor with `post_author` data when the post is created.
  */
 class LLMS_Blocks_Post_Instructors {
 
 	/**
+	 * Supported Post Types.
+	 *
+	 * @var array
+	 */
+	protected $post_types = array( 'course', 'llms_membership' );
+
+	/**
 	 * Constructor.
 	 *
-	 * @since    1.0.0
-	 * @version  1.0.0
+	 * @since 1.0.0
+	 *
+	 * @return void
 	 */
 	public function __construct() {
 
 		add_action( 'init', array( $this, 'register_meta' ) );
+		add_action( 'save_post_course', array( $this, 'maybe_set_default_instructor' ), 50, 3 );
+		add_action( 'save_post_llms_membership', array( $this, 'maybe_set_default_instructor' ), 50, 3 );
 
 	}
 
 	/**
 	 * Meta field update authorization callback.
+	 *
+	 * @since   1.0.0
 	 *
 	 * @param   bool   $allowed   Is the update allowed.
 	 * @param   string $meta_key  Meta keyname.
@@ -36,8 +52,6 @@ class LLMS_Blocks_Post_Instructors {
 	 * @param   string $cap       requested capability.
 	 * @param   array  $caps      user capabilities.
 	 * @return  bool
-	 * @since   1.0.0
-	 * @version 1.0.0
 	 */
 	public function authorize_callback( $allowed, $meta_key, $object_id, $user_id, $cap, $caps ) {
 		return user_can( $user_id, 'edit_post', $object_id );
@@ -46,11 +60,11 @@ class LLMS_Blocks_Post_Instructors {
 	/**
 	 * Retrieve instructor information for a give object.
 	 *
+	 * @since   1.0.0
+	 *
 	 * @param   array           $obj  Assoc. array of WP_Post data.
 	 * @param   WP_REST_Request $request   Full details about the request.
 	 * @return  WP_Error|object Object containing the meta values by name, otherwise WP_Error object.
-	 * @since   1.0.0
-	 * @version 1.0.0
 	 */
 	public function get_callback( $obj, $request ) {
 
@@ -73,14 +87,44 @@ class LLMS_Blocks_Post_Instructors {
 	}
 
 	/**
+	 * Automatically sets instructor data when a new course/membership is created.
+	 *
+	 * @since [version]
+	 *
+	 * @link https://developer.wordpress.org/reference/hooks/save_post_post-post_type/
+	 *
+	 * @param int     $post_id WP_Post ID.
+	 * @param WP_Post $post Post object.
+	 * @param bool    $update Whether the save is an update (`true`) or a creation (`false`).
+	 * @return bool
+	 */
+	public function maybe_set_default_instructor( $post_id, $post, $update ) {
+
+		if ( $update || ! $post->post_author ) {
+			return false;
+		}
+
+		$obj = llms_get_post( $post );
+		if ( ! $obj || ! is_a( $obj, 'LLMS_Post_Model' ) || ! in_array( $obj->get( 'type' ), $this->post_types, true ) ) {
+			return false;
+		}
+
+		remove_action( 'save_post_course', array( $this, 'maybe_set_instructors' ), 50, 3 );
+		$obj->instructors()->set_instructors( array( array( 'id' => $post->post_author ) ) );
+
+		return true;
+
+	}
+
+	/**
 	 * Update instructor information for a given object.
+	 *
+	 * @since   1.0.0
 	 *
 	 * @param   string  $value  Instructor data to add to the object (JSON).
 	 * @param   WP_Post $object WP_Post object.
 	 * @param   string  $key    name of the field.
 	 * @return  null|WP_Error
-	 * @since   1.0.0
-	 * @version 1.0.0
 	 */
 	public function update_callback( $value, $object, $key ) {
 
@@ -106,13 +150,14 @@ class LLMS_Blocks_Post_Instructors {
 	/**
 	 * Register custom meta fields.
 	 *
+	 * @since 1.0.0
+	 * @since [version] Use `$this->post_types` for loop.
+	 *
 	 * @return  void
-	 * @since   1.0.0
-	 * @version 1.0.0
 	 */
 	public function register_meta() {
 
-		foreach ( array( 'course', 'llms_membership' ) as $post_type ) {
+		foreach ( $this->post_types as $post_type ) {
 
 			register_rest_field(
 				$post_type,
