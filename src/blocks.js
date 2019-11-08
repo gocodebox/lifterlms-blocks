08de8d732678646a0533aaca6d4ef2ccce207579
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-11-08 09:59:09 -0800

    refactor all logic into scoped modules

--- src/blocks.js
+++ src/blocks.js
@@ -4,203 +4,21 @@
  * @since 1.0.0
  * @since 1.5.0 Register blocks conditionally based on post type.
  * @since 1.6.0 Register form field blocks.
+ * @since [version] Refactor into multiple files for clarity and organization.
  */
 
-// WP Deps.
-const
-	{
-		getBlockTypes,
-		registerBlockType,
-		unregisterBlockType,
-	} = wp.blocks,
-	{
-		subscribe,
-		select,
-	} = wp.data,
-	{ doAction } = wp.hooks;
-
-// Import jQuery.
-import $ from 'jquery';
-
-// Import Sidbear.
+// Internal Deps.
+import './block-visibility/'
+import './dom-ready/';
+import './post-visibility/'
+import './formats/merge-codes/';
 import './sidebar/'
 
-import Components from './components';
-window.llms.components = Components;
-
-// Import Blocks.
-import * as courseContinueButton from './blocks/course-continue-button/'
-import * as courseInfo from './blocks/course-information/'
-import * as courseProgress from './blocks/course-progress/'
-import * as courseSyllabus from './blocks/course-syllabus/'
-import * as instructors from './blocks/instructors/'
-import * as lessonNavigation from './blocks/lesson-navigation/'
-import * as lessonProgression from './blocks/lesson-progression/'
-import * as pricingTable from './blocks/pricing-table/'
-
-// Import form field blocks.
-import * as formFields from './blocks/form-fields/';
-
-// Add block visibility filters.
-import './block-visibility/'
-
-// Add block visibility filters.
-import './post-visibility/'
-
-// Add format buttons.
-import './formats/merge-codes/';
-
-/**
- * Retrieve the current post type from LifterLMS script data.
- *
- * @since 1.5.0
- *
- * @return string|false
- */
-const get_current_post_type = () => {
-
-	if ( window.llms && window.llms.post && window.llms.post.post_type ) {
-		return window.llms.post.post_type;
-	}
-
-	return false;
-}
-
-/**
- * Register LifterLMS Core Blocks
- *
- * @since 1.0.0
- * @since 1.5.0 Only register blocks for supported post types.
- * @since 1.6.0 Add form field blocks.
- *
- * @return  void
- */
-const registerBlocks = () => {
-
-	const post_type = get_current_post_type();
-
-	// Blocks to register.
-	let blocks = [
-		courseContinueButton,
-		courseInfo,
-		courseProgress,
-		courseSyllabus,
-		instructors,
-		lessonNavigation,
-		lessonProgression,
-		pricingTable,
-	];
-
-	// Add "composed" form fields to the block registration list.
-	Object.keys( formFields ).forEach( ( key ) => {
-
-		if ( formFields[ key ].composed ) {
-			blocks.push( formFields[ key ] );
-		}
-
-	} );
-
-	blocks.forEach( ( block ) => {
-
-		const {
-			name,
-			post_types,
-			settings,
-		} = block;
-
-		if ( ! post_types || -1 !== post_types.indexOf( post_type ) ) {
-			registerBlockType( name, settings );
-		}
-
-	} );
-};
+// Register all Core Blocks.
+import registerBlocks from './blocks/';
 registerBlocks();
 
-/**
- * Deregister all blocks no on a whitelist on the forms post type.
- *
- * @since 1.6.0
- * @since [version] Block 'llms/form-field-redeem-voucher' only available on registration forms.
- *
- * @return {void}
- */
-const deregisterBlocksForForms = () => {
-
-	/**
-	 * Determine if a block should be deregistered from form posts.
-	 *
-	 * @since [version]
-	 *
-	 * @param {String} name Block name.
-	 * @return {Boolean}
-	 */
-	const shouldUnregisterBlock = ( name ) => {
-
-		const whitelist = [ 'core/paragraph', 'core/heading', 'core/html', 'core/column', 'core/columns', 'core/group', 'core/separator', 'core/spacer' ];
-
-		// Allow whitelisted blocks.
-		if ( -1 !== whitelist.indexOf( name ) ) {
-			return false;
-
-			// Vouchers can only be used on registration forms.
-		} else if ( 0 === name.indexOf( 'llms/form-field-redeem-voucher' ) ) {
-
-			const { _llms_form_location } = select( 'core/editor' ).getCurrentPost().meta;
-			return 'registration' === _llms_form_location ? false : true;
-
-			// Allow all other form field blocks.
-		} else if ( -1 !== name.indexOf( 'llms/form-field' ) ) {
-			return false;
-		}
-
-		// unregister everything else.
-		return true;
-
-	};
-
-	getBlockTypes().forEach( ( { name } ) => {
-		if ( shouldUnregisterBlock( name ) ) {
-			unregisterBlockType( name );
-		}
-	} );
-
-};
-
-/**
- * On editor dom ready.
- *
- * @since 1.6.0
- *
- * @return {void}
- */
-wp.domReady( () => {
-
-	if ( 'llms_form' === get_current_post_type() ) {
-
-		deregisterBlocksForForms();
-
-		// Hide the "Switch to Draft" button in the header.
-		let saved = true;
-		subscribe( () => {
-
-			const
-				editor = select( 'core/editor' ),
-				isSaving = editor.isSavingPost() || editor.isPublishingPost();
-
-			if ( saved && ! isSaving ) {
-				$( '.edit-post-layout button.editor-post-switch-to-draft' ).hide();
-				saved = false;
-			}
-
-			saved = isSaving ? true : false;
-
-		} );
-
-		doAction( 'llms_form_fields_ready', formFields );
-
-	}
-
-} );
-
-
+// Import core Components and expose them for 3rd parties to utilize.
+import Components from './components/';
+window.llms.components = Components;
 
