6fac7eb1fbeb5532c234b94e06f698a370cc2ad9
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-12-05 12:25:00 -0800

    add a filter on the return of the check

--- src/block-visibility/check.js
+++ src/block-visibility/check.js
@@ -2,6 +2,7 @@
  * Determine if a block should have visibility settings added to it.
  *
  * @since 1.6.0
+ * @since [version] Add filter, `llms_block_supports_visibility` to allow modification of the return of the check.
  *
  * @param {Object} settings Block settings object.
  * @param {String} name Block name, eg "core/paragraph".
@@ -9,15 +10,20 @@
  */
 export default function supportsVisibility( settings, name ) {
 
+	// WP Deps.
+	const { applyFilters } = wp.hooks;
+
+	let ret = true;
+
 	// Don't add to Dynamic Blocks.
 	if ( -1 !== window.llms.dynamic_blocks.indexOf( name ) ) {
-		return false;
+		ret = false;
 
 	// Don't add to blocks that explicitly don't support llms_visibility.
 	} else if ( settings.supports && false === settings.supports.llms_visibility ) {
-		return false;
+		ret = false;
 	}
 
-	return true;
+	return applyFilters( 'llms_block_supports_visibility', ret, settings, name );
 
 }
