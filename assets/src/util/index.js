ef7a6a5ff7896412841f9edf254f7142aef29e7c
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-04-24 10:11:12 -0700

    cs

--- assets/src/util/index.js
+++ assets/src/util/index.js
@@ -5,15 +5,7 @@
  * @version 1.7.0
  */
 
-import { flattenBlocks } from './get-blocks-flat';
-import getBlocksFlat from './get-blocks-flat';
+import getBlocksFlat, { flattenBlocks } from './get-blocks-flat';
 import getCurrentPostType from './get-current-post-type.js';
 
-export {
-
-	getBlocksFlat,
-	getCurrentPostType,
-
-	flattenBlocks,
-
-};
+export { getBlocksFlat, getCurrentPostType, flattenBlocks };
