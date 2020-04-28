a35e74f88ff1f8a588c150f6aba615e8b08dc465
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-04-28 14:53:55 -0700

    build: 1.8.0

--- assets/js/llms-blocks.asset.php
+++ assets/js/llms-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '6e3378542aa03105da89f29d7aa8eb1e');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '0b0258bdb32d23c4497fde29aaa6ac56');
