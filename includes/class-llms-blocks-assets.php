1b1026d8d0b688506dcb4b83faebc77cbaf11504
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-11-23 16:46:51 -0700

    Use LLMS_Assets for asset registration and script localization

--- includes/class-llms-blocks-assets.php
+++ includes/class-llms-blocks-assets.php
@@ -18,72 +18,103 @@ defined( 'ABSPATH' ) || exit;
  * @since 1.0.0
  * @since 1.4.1 Fix double slash in asset path; remove invalid frontend css dependency.
  * @since 1.8.0 Update asset paths & remove redundant CSS from frontend.
+ * @since [version] Use the `LLMS_Assets` class to define, register, and enqueue plugin assets.
  */
 class LLMS_Blocks_Assets {
+
+	/**
+	 * Instances of `LLMS_Assets`
+	 *
+	 * @var null
+	 */
+	public $assets;
 
 	/**
 	 * Constructor
 	 *
 	 * @since 1.0.0
 	 * @since 1.8.0 Stop outputting editor CSS on the frontend.
+	 * @since [version] Load `LLMS_Assets` and define plugin assets.
 	 *
 	 * @return void
 	 */
 	public function __construct() {
+
+		// Load an instance of the LLMS_Assets class.
+		$this->assets = new LLMS_Assets(
+			'llms-blocks',
+			array(
+				// Base defaults shared by all asset types.
+				'base'   => array(
+					'base_file' => LLMS_BLOCKS_PLUGIN_FILE,
+					'base_url'  => LLMS_BLOCKS_PLUGIN_DIR_URL,
+					'version'   => LLMS_BLOCKS_VERSION,
+					'suffix'    => '', // Only minified files are distributed.
+				),
+				// Script specific defaults.
+				'script' => array(
+					'translate' => true, // All scripts in the blocks plugin are translated.
+				),
+			)
+		);
+
+		// Define plugin assets.
+		$this->define();
+
+		// Enqueue editor assets.
 		add_action( 'enqueue_block_editor_assets', array( $this, 'editor_assets' ), 999 );
+
 	}
 
 	/**
-	 * Enqueue Gutenberg block assets for backend editor.
+	 * Define block plugin assets.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	protected function define() {
+
+		$asset = include LLMS_BLOCKS_PLUGIN_DIR . '/assets/js/llms-blocks.asset.php';
+
+		$this->assets->define(
+			'scripts',
+			array(
+				'llms-blocks-editor' => array(
+					'dependencies' => $asset['dependencies'],
+					'file_name'    => 'llms-blocks',
+					'version'      => $asset['version'],
+				),
+			)
+		);
+
+		$this->assets->define(
+			'styles',
+			array(
+				'llms-blocks-editor' => array(
+					'dependencies' => array( 'wp-edit-blocks' ),
+					'file_name'    => 'llms-blocks',
+					'version'      => $asset['version'],
+				),
+			)
+		);
+
+	}
+
+	/**
+	 * Enqueue block editor assets.
 	 *
 	 * @since 1.0.0
 	 * @since 1.4.1 Fix double slash in asset path.
 	 * @since 1.8.0 Update asset paths and improve script dependencies.
-	 * @since [version] Load script translations & add RTL css.
+	 * @since [version] Use `LLMS_Assets` class methods for asset enqueues.
 	 *
 	 * @return void
 	 */
 	public function editor_assets() {
 
-		$asset = include LLMS_BLOCKS_PLUGIN_DIR . '/assets/js/llms-blocks.asset.php';
-
-		wp_enqueue_script(
-			'llms-blocks-editor',
-			LLMS_BLOCKS_PLUGIN_DIR_URL . 'assets/js/llms-blocks.js',
-			$asset['dependencies'],
-			$asset['version'],
-			true
-		);
-
-		$l10n_dir   = 'i18n';
-		$plugin_dir = LLMS_BLOCKS_PLUGIN_DIR;
-
-		// If the plugin is being loaded as a library, use the files included in the core plugin.
-		if ( defined( 'LLMS_BLOCKS_LIB' ) && LLMS_BLOCKS_LIB ) {
-			$l10n_dir   = null;
-			$plugin_dir = null;
-		}
-
-		/**
-		 * Load script translations.
-		 *
-		 * Language files files can be found in the following locations (The first loaded file takes priority):
-		 *
-		 *   1. wp-content/languages/lifterlms/lifterlms-{$locale}-{md5($handle)}.json
-		 *   2. wp-content/languages/plugins/lifterlms-{$locale}-{md5($handle)}.json
-		 *   3. wp-content/plugins/lifterlms/languages/lifterlms-{$locale}-{md5($handle)}.json
-		 *
-		 * See `llms_set_script_translations()` for more information.
-		 */
-		llms_set_script_translations( 'llms-blocks-editor', 'lifterlms', $plugin_dir, $l10n_dir );
-
-		wp_enqueue_style(
-			'llms-blocks-editor',
-			LLMS_BLOCKS_PLUGIN_DIR_URL . 'assets/css/llms-blocks.css',
-			array( 'wp-edit-blocks' ),
-			$asset['version']
-		);
-		wp_style_add_data( 'llms-blocks-editor', 'rtl', 'replace' );
+		$this->assets->enqueue_script( 'llms-blocks-editor' );
+		$this->assets->enqueue_style( 'llms-blocks-editor' );
 
 	}
 
