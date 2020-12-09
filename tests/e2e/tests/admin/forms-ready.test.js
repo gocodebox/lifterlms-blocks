3ef21ddb1a3a29eb4f0c230be592d2dd8a26d597
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-12-09 16:36:21 -0700

    add tests for forms ready stuff

---
+++ tests/e2e/tests/admin/forms-ready.test.js
@@ -0,0 +1,122 @@
+/**
+ * Test formsReady() events run on domReady()
+ *
+ * @since [version]
+ * @version [version]
+ */
+
+import {
+	getAllBlocks,
+	getAllBlockInserterItemTitles,
+	insertBlock,
+	openGlobalBlockInserter,
+	openDocumentSettingsSidebar,
+	publishPostWithPrePublishChecksDisabled,
+	selectBlockByClientId,
+} from '@wordpress/e2e-test-utils';
+
+import {
+	click,
+	clickElementByText,
+} from '@lifterlms/llms-e2e-test-utils';
+
+import {
+	visitForm,
+} from '../../util';
+
+async function removeUserEmailBlock() {
+
+	const blocks = await getAllBlocks();
+	await selectBlockByClientId( blocks[0].clientId );
+
+	await click( 'button.components-dropdown-menu__toggle[aria-label="More options"]' );
+	await clickElementByText( 'Remove block' );
+
+}
+
+
+describe( 'Admin/FormsReady', () => {
+
+	it ( 'should disable the use of the "Draft" button', async () => {
+
+		await visitForm();
+
+		// On page load.
+		await page.waitFor( 5 );
+		expect( await page.$eval( '.edit-post-layout button.editor-post-switch-to-draft', el => el.style.display ) ).toBe( 'none' );
+
+		// Add a block.
+		await insertBlock( 'Paragraph' );
+		await page.waitFor( 5 );
+		expect( await page.$eval( '.edit-post-layout button.editor-post-switch-to-draft', el => el.style.display ) ).toBe( 'none' );
+
+		// After updating.
+		await publishPostWithPrePublishChecksDisabled();
+		await page.waitFor( 5 );
+		expect( await page.$eval( '.edit-post-layout button.editor-post-switch-to-draft', el => el.style.display ) ).toBe( 'none' );
+
+	} );
+
+	it ( 'should disable the "Status & Visibility" sidebar panel', async () => {
+
+		await visitForm();
+
+		// On page load.
+		await page.waitFor( 5 );
+		expect( await page.$eval( '.edit-post-layout .components-panel__body.edit-post-post-status', el => el.style.display ) ).toBe( 'none' );
+
+		// Open Sidebar
+		await openDocumentSettingsSidebar();
+		await page.waitFor( 5 );
+		expect( await page.$eval( '.edit-post-layout .components-panel__body.edit-post-post-status', el => el.style.display ) ).toBe( 'none' );
+
+		// Close & Open again.
+		await openDocumentSettingsSidebar();
+		await page.waitFor( 5 );
+		await openDocumentSettingsSidebar();
+		expect( await page.$eval( '.edit-post-layout .components-panel__body.edit-post-post-status', el => el.style.display ) ).toBe( 'none' );
+
+	} );
+
+	it ( 'should add a notice and disable post updating when user email field is deleted', async () => {
+
+		page.on( 'dialog', dialog => dialog.accept() ); // Leave page without saving.
+
+		await visitForm();
+		await removeUserEmailBlock();
+		await page.waitForSelector( '.components-notice.is-error' );
+
+		expect( await page.$eval( '.components-notice.is-error .components-notice__content', el => el.innerHTML ) ).toMatchSnapshot();
+
+		expect( await page.$eval( 'button.editor-post-publish-button', el => el.disabled ) ).toBe( true );
+
+	} );
+
+	it ( 'should restore the user email field when the "Restore" CTA is used in the notice', async () => {
+
+		await visitForm();
+		await removeUserEmailBlock();
+		await page.waitForSelector( '.components-notice.is-error' );
+
+		await click( '.components-notice.is-error .components-notice__content button' );
+
+		// Notice gets removed
+		expect( await page.evaluate( () => document.querySelector( '.components-notice.is-error' ) ) ).toBeNull();
+		const blocks = await getAllBlocks();
+		expect( blocks[0].name ).toBe( 'llms/form-field-user-email' );
+
+		// Can be updated.
+		expect( await page.$eval( 'button.editor-post-publish-button', el => el.disabled ) ).toBe( false );
+
+	} );
+
+	it ( 'should deregister most WP core blocks', async () => {
+
+		await visitForm();
+
+		await openGlobalBlockInserter();
+		expect( await getAllBlockInserterItemTitles() ).toMatchSnapshot();
+
+	} );
+
+} );
---
+++ tests/e2e/util/index.js
@@ -0,0 +1,12 @@
+/**
+ * E2E Utilities functions
+ *
+ * @package LifterLMS_Blocks/Tests/E2E/Utils
+ *
+ * @since [version]
+ * @version [version]
+ */
+
+import visitForm from './visit-form';
+
+export { visitForm };
