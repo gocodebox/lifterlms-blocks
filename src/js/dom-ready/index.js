82609ed92a9f4124d56dd498b90efc550322d4ee
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-12-09 16:39:18 -0700

    Wait for core post init before executing callback

--- src/js/dom-ready/index.js
+++ src/js/dom-ready/index.js
@@ -2,30 +2,47 @@
  * Handle DOM Ready Events.
  *
  * @since 1.7.0
- * @version 1.7.0
+ * @version [version]
  */
 
 // WP Deps.
-const { domReady } = wp;
+import domReady from '@wordpress/dom-ready';
+import {
+	select,
+	subscribe,
+} from '@wordpress/data';
 
 // Internal Deps.
 import formsReady from './forms-ready';
-import { getCurrentPostType } from '../util/';
-
-
 
 /**
  * On editor DOM ready.
  *
  * @since 1.6.0
  * @since 1.7.0 Refactor for simplicity.
+ * @since [version] Wait for current post to be setup before dispatching ready event.
  *
  * @return {void}
  */
 domReady( () => {
 
-	if ( 'llms_form' === getCurrentPostType() ) {
-		formsReady();
-	}
+	/**
+	 * The unsubscribe seems to not run "fast" enough and we end up calling the formsReady() method twice
+	 * when relying solely on unsubscribe(). Adding a boolean check appears to "fix" this "problem".
+	 */
+	let dispatched = false;
+
+	const unsubscribe = subscribe( () => {
+
+		const post = select( 'core/editor' ).getCurrentPost();
+		if ( false === dispatched && 0 !== Object.keys( post ).length && 'llms_form' === post.type ) {
+
+			dispatched = true;
+			unsubscribe();
+			formsReady();
+
+		}
+
+	} );
 
 } );
