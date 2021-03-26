20e91efb6dbc09b57e48eab151378f2a51ca0f36
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-03-26 15:39:47 -0700

    Improve public api and accommodate reusable blocks

--- src/js/blocks/form-fields/checks.js
+++ src/js/blocks/form-fields/checks.js
@@ -1,44 +1,109 @@
 /**
- * ??
+ * Form Field related checks
  *
  * @since Unknown
- * @since 1.8.0 Updated lodash imports.
+ * @version [version]
  */
 
-import { select } from '@wordpress/data';
-
+// External Deps.
 import { every, forEach } from 'lodash';
 
-function getFieldBlocks() {
-	const ret = [];
+// WP Deps.
+import { select } from '@wordpress/data';
+import { useEntityBlockEditor } from '@wordpress/core-data';
 
-	const checkBlock = ( block ) => {
-		if ( block.innerBlocks.length ) {
-			return checkBlocks( block.innerBlocks );
+/**
+ * Determines if the block is (or contains) field block/s
+ *
+ * If the block contains innerBlocks (or is a reusable block) it
+ * passes the list of contained to filterBlocks.
+ *
+ * @since Unknown
+ * @since [version] Adds support for reusable blocks.
+ *
+ * @param {WP_Block} block A block object.
+ * @return {WP_Block[]} An array of field blocks.
+ */
+function filterBlock( block ) {
+
+	// Check the block's inner blocks.
+	if ( block.innerBlocks.length ) {
+		return filterBlocks( block.innerBlocks );
+	}
+
+	// Reusable blocks don't have inner blocks defined so we need to look them up this ay.
+	if ( 'core/block' === block.name ) {
+
+		const { blocks } = select( 'core' ).getEditedEntityRecord( 'postType', 'wp_block', block.attributes.ref );
+		return filterBlocks( blocks );
+
+	}
+
+	// Only return form field blocks.
+	if ( -1 === block.name.indexOf( 'llms/form-field' ) ) {
+		return [];
+	}
+
+	return [ block ];
+
+};
+
+/**
+ * Recursively filter blocks (checking inner blocks)
+ *
+ * Returns only a (flattened) list of field blocks.
+ *
+ * @since [version]
+ *
+ * @param {WP_Block[]} blocks An array of blocks to filter.
+ * @return {WP_Block[]} An array of field blocks.
+ */
+function filterBlocks( blocks = [] ) {
+
+	let fieldBlocks = [];
+
+	forEach( blocks, ( block ) => {
+
+		const filtered = filterBlock( block );
+		if ( filtered.length ) {
+			fieldBlocks = fieldBlocks.concat( filtered );
 		}
 
-		if ( -1 === block.name.indexOf( 'llms/form-field' ) ) {
-			return false;
-		}
+	} );
 
-		return block;
-	};
+	return fieldBlocks;
 
-	const checkBlocks = ( blocks ) => {
-		forEach( blocks, ( block ) => {
-			if ( checkBlock( block ) ) {
-				ret.push( block );
-			}
-		} );
-	};
+};
 
-	checkBlocks( select( 'core/block-editor' ).getBlocks() );
+/**
+ * Retrieve a "flattened" list of all form fields from a list of blocks
+ *
+ * Recursively checks each block for it's inner blocks to determine if there's
+ * fields in the block.
+ *
+ * @since Unknown
+ *
+ * @param {WP_Block[]} blocks An array of blocks to filter.
+ * @return {WP_Block[]} An array of field blocks.
+ */
+export const getFieldBlocks = ( blocks = [] ) => {
+	if ( ! Array.isArray( blocks ) ) {
+		blocks = [ blocks ];
+	}
+	return filterBlocks( blocks );
+};
 
-	return ret;
-}
-
+/**
+ * Ensure field attributes are unique across the entire form
+ *
+ * @since Unknown
+ *
+ * @param {String} field Attribute key name.
+ * @param {String} str   String to check for uniqueness.
+ * @return {Boolean} Returns `true` when the string is unique across the form and `false` if it's not.
+ */
 export const isUnique = ( field, str ) => {
-	return every( getFieldBlocks(), ( block ) => {
+	return every( getFieldBlocks( select( 'core/block-editor' ).getBlocks() ), ( block ) => {
 		return block.attributes[ field ] !== str;
 	} );
 };
