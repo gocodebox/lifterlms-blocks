c6496649825d780ca74bd8ceee63f2682e66ae89
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-04-08 13:30:46 -0700

    adds a user display name field, resolves https://github.com/gocodebox/lifterlms/issues/691

---
+++ src/js/blocks/form-fields/fields/user-display-name.js
@@ -0,0 +1,81 @@
+/**
+ * BLOCK: llms/form-field-user-display-name
+ *
+ * @since [version]
+ * @version [version]
+ */
+
+// WP Deps.
+import { __ } from '@wordpress/i18n';
+
+// Internal Deps.
+import { settings as baseSettings, postTypes } from './text';
+import { getSettingsFromBase } from '../settings';
+
+/**
+ * Block Name
+ *
+ * @type {string}
+ */
+export const name = 'llms/form-field-user-display-name';
+
+/**
+ * Is this a default or composed field?
+ *
+ * @type {string}
+ */
+export const composed = true;
+
+/**
+ * Block settings
+ *
+ * @since [version]
+ *
+ * @type {Object}
+ */
+export const settings = getSettingsFromBase(
+	baseSettings,
+	{
+		title: __( 'User Display Name', 'lifterlms' ),
+		description: __(
+			"Allows a user to choose how their name will be displayed publicly on the site.",
+			'lifterlms'
+		),
+		icon: {
+			src: 'nametag',
+		},
+		supports: {
+			multiple: false,
+			llms_field_inspector: {
+				id: false,
+				name: false,
+				required: false,
+				storage: false,
+			},
+		},
+		attributes: {
+			id: {
+				__default: 'display_name',
+			},
+			field: {
+				__default: 'text',
+			},
+			label: {
+				__default: __( 'Email Address', 'lifterlms' ),
+			},
+			name: {
+				__default: 'display_name',
+			},
+			data_store: {
+				__default: 'users',
+			},
+			data_store_key: {
+				__default: 'display_name',
+			},
+
+		},
+	},
+	[ 'transforms', 'variations' ],
+);
+
+export { postTypes }
