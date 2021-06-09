79c996765e150db54cad2834a3e8fbe4959602c0
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-06-09 15:36:38 -0700

    Update isUnique() to utilize llms/user-info-fields store data

--- src/js/blocks/form-fields/checks.js
+++ src/js/blocks/form-fields/checks.js
@@ -11,6 +11,9 @@ import { every, forEach } from 'lodash';
 // WP Deps.
 import { select } from '@wordpress/data';
 
+// Internal deps.
+import { store as fieldsStore } from '../../data/fields';
+
 /**
  * Determines if the block is (or contains) field block/s
  *
@@ -94,19 +97,20 @@ export const getFieldBlocks = ( blocks = [] ) => {
 };
 
 /**
- * Ensure field attributes are unique across the entire form
+ * Ensure field attributes are unique in the requested context
  *
  * @since Unknown
+ * @since [version] Added `context` parameter and use data from the llms/user-info-fields store.
  *
- * @param {string} field Attribute key name.
- * @param {string} str   String to check for uniqueness.
- * @return {boolean} Returns `true` when the string is unique across the form and `false` if it's not.
+ * @param {string} key     Attribute key name.
+ * @param {string} val     String to check for uniqueness.
+ * @param {string} context Field context to look within. Accepts "global" to check against all fields or "local"
+ *                         to check only against loaded fields in the current form.
+ * @return {Boolean} Returns `true` when the string is unique across the form and `false` if it's not.
  */
-export const isUnique = ( field, str ) => {
-	return every(
-		getFieldBlocks( select( 'core/block-editor' ).getBlocks() ),
-		( block ) => {
-			return block.attributes[ field ] !== str;
-		}
-	);
+export const isUnique = ( key, val, context = 'global' ) => {
+
+	const { getFieldBy } = select( fieldsStore );
+	return getFieldBy( key, val, context ) ? false : true;
+
 };
