1765789a975313f38982fd8922ea90bef5f9f627
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-06-15 15:01:04 -0700

    eslint

--- src/js/blocks-backwards-compat.js
+++ src/js/blocks-backwards-compat.js
@@ -10,12 +10,12 @@ import { createReduxStore, register } from 'llmsWPData';
 
 // Make text definitions of all the data stores we reference via import.
 window.wp.blockEditor.store = 'core/block-editor';
-window.wp.editor.store      = 'core/editor';
-window.wp.notices.store     = 'core/notices';
+window.wp.editor.store = 'core/editor';
+window.wp.notices.store = 'core/notices';
 
 // Ensure we can register data stores.
 window.wp.data = {
 	...window.wp.data,
 	createReduxStore,
 	register,
-}
+};
