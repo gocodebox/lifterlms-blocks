cc9b7db89e484c8e3643892e6b651a561ee2993a
seothemes <seothemeswp@gmail.com>
2023-04-17 20:59:11 +0800

    Deprecate all methods in course syllabus block

--- includes/blocks/class-llms-blocks-course-syllabus-block.php
+++ includes/blocks/class-llms-blocks-course-syllabus-block.php
@@ -2,13 +2,11 @@
 /**
  * Course syllabus block.
  *
- * @package  LifterLMS_Blocks/Blocks
+ * @package LifterLMS_Blocks/Blocks
  *
- * @since    1.0.0
- * @version  1.1.0
+ * @since 1.0.0
+ * @version 1.1.0
  * @deprecated [version]
- *
- * @render_hook llms_course-syllabus-block_render
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -28,23 +26,24 @@ class LLMS_Blocks_Course_Syllabus_Block extends LLMS_Blocks_Abstract_Block {
 	/**
 	 * Is block dynamic (rendered in PHP).
 	 *
-	 * @var bool
 	 * @since [version] Changed to `false` to prevent the block from being registered.
+	 *
+	 * @var bool
 	 */
 	protected $is_dynamic = false;
 
 	/**
 	 * Add actions attached to the render function action.
 	 *
-	 * @param   array  $attributes Optional. Block attributes. Default empty array.
-	 * @param   string $content    Optional. Block content. Default empty string.
-	 * @return  void
-	 * @since   1.0.0
-	 * @version 1.1.0
+	 * @since 1.0.0
 	 * @deprecated [version]
+	 *
+	 * @param array  $attributes Optional. Block attributes. Default empty array.
+	 * @param string $content Optional. Block content. Default empty string.
+	 * @return void
 	 */
 	public function add_hooks( $attributes = array(), $content = '' ) {
-		llms_deprecated_function( __METHOD__, '1.2.0', __CLASS__ . '::add_hooks()' );
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::add_hooks()' );
 
 		add_action( $this->get_render_hook(), 'lifterlms_template_single_syllabus', 10 );
 	}
@@ -53,13 +52,13 @@ class LLMS_Blocks_Course_Syllabus_Block extends LLMS_Blocks_Abstract_Block {
 	 * Retrieve custom block attributes.
 	 * Necessary to override when creating ServerSideRender blocks.
 	 *
-	 * @return  array
-	 * @since   1.0.0
-	 * @version 1.0.0
+	 * @since 1.0.0
 	 * @deprecated [version]
+	 *
+	 * @return array
 	 */
 	public function get_attributes() {
-		llms_deprecated_function( __METHOD__, '1.2.0', __CLASS__ . '::get_attributes()' );
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::get_attributes()' );
 
 		return array_merge(
 			parent::get_attributes(),
@@ -72,6 +71,107 @@ class LLMS_Blocks_Course_Syllabus_Block extends LLMS_Blocks_Abstract_Block {
 		);
 	}
 
+	/**
+	 * Retrieve the ID/Name of the block.
+	 *
+	 * @since 1.0.0
+	 * @deprecated [version]
+	 *
+	 * @return string
+	 */
+	public function get_block_id() {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::get_block_id()' );
+
+		return sprintf( '%1$s/%2$s', $this->vendor, $this->id );
+	}
+
+	/**
+	 * Output a message when no HTML was rendered.
+	 *
+	 * @since 1.0.0
+	 * @since 1.8.0 Don't output empty render messages on the frontend.
+	 * @deprecated [version]
+	 *
+	 * @return string
+	 */
+	public function get_empty_render_message() {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::get_empty_render_message()' );
+
+		if ( ! is_admin() ) {
+			return '';
+		}
+
+		return __( 'No HTML was returned.', 'lifterlms' );
+	}
+
+	/**
+	 * Retrieve a string which can be used to render the block.
+	 *
+	 * @since 1.0.0
+	 * @deprecated [version]
+	 *
+	 * @return string
+	 */
+	public function get_render_hook() {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::get_render_hook()' );
+
+		return sprintf( '%1$s_%2$s_block_render', $this->vendor, $this->id );
+	}
+
+	/**
+	 * Removed hooks stub.
+	 * Extending classes can use this class to remove hooks attached to the render function action.
+	 *
+	 * @since 1.0.0
+	 * @deprecated [version]
+	 *
+	 * @return void
+	 */
+	public function remove_hooks() {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::remove_hooks()' );
+	}
+
+	/**
+	 * Renders the block type output for given attributes.
+	 *
+	 * @since 1.0.0
+	 * @deprecated [version]
+	 *
+	 * @param array  $attributes Optional. Block attributes. Default empty array.
+	 * @param string $content Optional. Block content. Default empty string.
+	 * @return string
+	 */
+	public function render_callback( $attributes = array(), $content = '' ) {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::render_callback()' );
+
+		$this->add_hooks( $attributes, $content );
+
+		ob_start();
+		do_action( $this->get_render_hook(), $attributes, $content );
+		$ret = ob_get_clean();
+
+		$this->remove_hooks();
+
+		if ( ! $ret ) {
+			$ret = $this->get_empty_render_message();
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * Register meta attributes stub.
+	 *
+	 * Called after registering the block type.
+	 *
+	 * @since 1.0.0
+	 * @deprecated [version]
+	 *
+	 * @return void
+	 */
+	public function register_meta() {
+		llms_deprecated_function( __METHOD__, '[version]', __CLASS__ . '::register_meta()' );
+	}
 }
 
 return new LLMS_Blocks_Course_Syllabus_Block();
