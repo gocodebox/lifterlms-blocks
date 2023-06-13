7c540ba0f667ac78a081aadf2491ac59f4f02b91
Rocco Aliberti <raliberti84@gmail.com>
2023-06-13 16:28:47 +0200

    Fixed Launch Course Builder buttons not working when WordPress was installed in a subdirectory

--- src/js/sidebar/course-builder/toolbar-launch-button.jsx
+++ src/js/sidebar/course-builder/toolbar-launch-button.jsx
@@ -1,8 +1,22 @@
+/**
+ * Tooblar launch button.
+ *
+ * @since 2.5.0
+ * @version [version]
+ */
+
 import { select } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 const buttonId = 'llms-launch-course-builder-top-button';
 
+/**
+ * Toolbar launch button component.
+ *
+ * @since 2.5.0
+ * @since [version] Fix button link using localized admin url so to avoid issues when
+ *               WordPress is installed in a subdirectory.
+ */
 export const addToolbarLaunchButton = () => {
 	const editPostHeaderToolbarLeft = document.getElementsByClassName(
 		'edit-post-header-toolbar__left'
@@ -24,7 +38,7 @@ export const addToolbarLaunchButton = () => {
 		const button = document.createElement( 'a' );
 
 		button.id = buttonId;
-		button.href = '/wp-admin/admin.php?page=llms-course-builder&course_id=' + courseId;
+		button.href = window.llms.admin_url + 'admin.php?page=llms-course-builder&course_id=' + courseId;
 		button.className = 'llms-button-primary';
 		button.style.marginLeft = '16px';
 		button.innerHTML = __( 'Launch Course Builder', 'lifterlms' );
