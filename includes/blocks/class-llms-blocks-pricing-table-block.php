599a2a0c54398d8501f7bc4897f22fcdcdb1cb5d
SEO Themes <seothemeswp@gmail.com>
2023-08-14 15:51:40 +0800

    Deprecate all methods of `LLMS_Blocks_Pricing_Table_Block` and `LLMS_Blocks_Course_Progress_Block`

--- includes/blocks/class-llms-blocks-pricing-table-block.php
+++ includes/blocks/class-llms-blocks-pricing-table-block.php
@@ -139,4 +139,100 @@ class LLMS_Blocks_Pricing_Table_Block extends LLMS_Blocks_Abstract_Block {
 		}
 
 	}
+
+	/**
+	 * Retrieve the ID/Name of the block.
+	 *
+	 * @return  string
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function get_block_id() {
+		llms_deprecated_function( __METHOD__, '[version]' );
+
+		return sprintf( '%1$s/%2$s', $this->vendor, $this->id );
+	}
+
+	/**
+	 * Output a message when no HTML was rendered
+	 *
+	 * @since 1.0.0
+	 * @since 1.8.0 Don't output empty render messages on the frontend.
+	 *
+	 * @return  string
+	 */
+	public function get_empty_render_message() {
+		llms_deprecated_function( __METHOD__, '[version]' );
+
+		if ( ! is_admin() ) {
+			return '';
+		}
+		return __( 'No HTML was returned.', 'lifterlms' );
+	}
+
+	/**
+	 * Retrieve a string which can be used to render the block.
+	 *
+	 * @return  string
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function get_render_hook() {
+		llms_deprecated_function( __METHOD__, '[version]' );
+
+		return sprintf( '%1$s_%2$s_block_render', $this->vendor, $this->id );
+	}
+
+	/**
+	 * Removed hooks stub.
+	 * Extending classes can use this class to remove hooks attached to the render function action.
+	 *
+	 * @return  void
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function remove_hooks() {
+		llms_deprecated_function( __METHOD__, '[version]' );
+	}
+
+	/**
+	 * Renders the block type output for given attributes.
+	 *
+	 * @param   array  $attributes Optional. Block attributes. Default empty array.
+	 * @param   string $content    Optional. Block content. Default empty string.
+	 * @return  string
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function render_callback( $attributes = array(), $content = '' ) {
+		llms_deprecated_function( __METHOD__, '[version]' );
+
+		$this->add_hooks( $attributes, $content );
+
+		ob_start();
+		do_action( $this->get_render_hook(), $attributes, $content );
+		$ret = ob_get_clean();
+
+		$this->remove_hooks();
+
+		if ( ! $ret ) {
+			$ret = $this->get_empty_render_message();
+		}
+
+		return $ret;
+
+	}
+
+	/**
+	 * Register meta attributes stub.
+	 *
+	 * Called after registering the block type.
+	 *
+	 * @return  void
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public function register_meta() {
+		llms_deprecated_function( __METHOD__, '[version]' );
+	}
 }
