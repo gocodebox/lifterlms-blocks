6195f4e10ac20dfed93d57937a9292ecbb04e0ee
Brian Hogg <brian@bhconsulting.ca>
2024-06-27 14:35:53 -0400

    Escaping.

--- includes/blocks/class-llms-blocks-pricing-table-block.php
+++ includes/blocks/class-llms-blocks-pricing-table-block.php
@@ -48,7 +48,6 @@ class LLMS_Blocks_Pricing_Table_Block extends LLMS_Blocks_Abstract_Block {
 	public function add_hooks( $attributes = array(), $content = '' ) {
 
 		add_action( $this->get_render_hook(), array( $this, 'output' ), 10 );
-
 	}
 
 	/**
@@ -120,9 +119,8 @@ class LLMS_Blocks_Pricing_Table_Block extends LLMS_Blocks_Abstract_Block {
 		remove_filter( 'llms_product_is_purchasable', '__return_true' );
 
 		if ( $block_content ) {
-			echo $block_content;
+			echo wp_kses_post( $block_content );
 		}
-
 	}
 }
 
