607efd6e9a8f5b02de3afa7f65a39fe1a4000da1
Brian Hogg <brian@bhconsulting.ca>
2025-01-17 09:33:58 -0500

    Release v2.5.9 [ci skip]

--- lifterlms-blocks.php
+++ lifterlms-blocks.php
@@ -11,7 +11,7 @@
  * Plugin Name: LifterLMS Blocks
  * Plugin URI: https://github.com/gocodebox/lifterlms-blocks
  * Description: WordPress Editor (Gutenberg) blocks for LifterLMS.
- * Version: 2.5.8
+ * Version: 2.5.9
  * Author: LifterLMS
  * Author URI: https://lifterlms.com/
  * Text Domain: lifterlms
@@ -27,7 +27,7 @@ defined( 'ABSPATH' ) || exit;
 
 // Define Constants.
 if ( ! defined( 'LLMS_BLOCKS_VERSION' ) ) {
-	define( 'LLMS_BLOCKS_VERSION', '2.5.8' );
+	define( 'LLMS_BLOCKS_VERSION', '2.5.9' );
 }
 
 /**
