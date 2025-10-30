80694abf271a2deb60d5f71074788e39dd21b611
Brian Hogg <brian@bhconsulting.ca>
2025-10-30 13:00:56 -0400

    Removing the estimated completion time box from the block.

--- src/js/blocks/course-information/inspect.js
+++ src/js/blocks/course-information/inspect.js
@@ -61,18 +61,6 @@ export default class Inspector extends Component {
 						] }
 					/>
 
-					<TextControl
-						label={ __( 'Estimated Completion Time', 'lifterlms' ) }
-						value={ length }
-						onChange={ ( value ) =>
-							setAttributes( { length: value } )
-						}
-						help={ __(
-							'How many hours, days, weeks, etc… should a student expect to spend in order to complete this course.',
-							'lifterlms'
-						) }
-					/>
-
 					<ToggleControl
 						label={ __( 'Display Estimated Time', 'lifterlms' ) }
 						checked={ !! show_length }
